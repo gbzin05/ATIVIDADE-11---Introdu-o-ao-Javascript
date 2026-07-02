6392e8529ba7b8175fbd1b7ff71f84734ba8351a
gbzin05 <gabrielalvescarvalho1038@gmail.com>
2026-07-02 16:04:29 -0300

    Update 06.js

--- 06.js
+++ 06.js
@@ -3,5 +3,5 @@ let valorOriginal = parseFloat(prompt("Digite o valor original do par de sapatos
 let desconto = valorOriginal * 0.15;
 let valorFinal = valorOriginal - desconto;
 
-alert("Valor do desconto: R$ " + desconto.toFixed(2));
-alert("Valor final a pagar: R$ " + valorFinal.toFixed(2));
+alert("Valor do desconto: R$ " + desconto
+alert("Valor final a pagar: R$ " + valorFinal
